50142f7358281ee655fa6fe5dc41ace8c4dea69f
Guilherme henrique machado <progr.guilherme@gmail.com>
2020-12-26 23:42:43 -0300

    feat: filter basic structure

--- components/Filter/Filter.tsx
+++ components/Filter/Filter.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Text, View, ScrollView, StyleSheet, SafeAreaView, StatusBar } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const Filter = () => {
   return (
     <View style={styles.container}>
-      <Text>Departamentos</Text>
+      <Text style={styles.containerTitle}>Departamentos</Text>
       <View style={styles.filters}>
         {['eletrônicos', 'Celulares e Telefones', 'Ferramentas e construção', 'bebês', 'Eletrônicos', 'brinquedos e hobbies'].map((item) => (
-          <View key={item}>
-            <Text>{item}</Text>
+          <View style={styles.filter} key={item}>
+            <Text style={styles.filterTitle}>{item}</Text>
           </View>
         ))}
       </View>
@@ -20,14 +20,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: '100%',
-    backgroundColor: '#242323',
     color: '#fff'
+  },
+  containerTitle: {
+    marginBottom: 10,
+    color: '#e5e5e5',
+    fontWeight: 'bold',
   },
   filters: {
     flex: 1,
-    backgroundColor: 'red',
     flexDirection: 'row',
     flexWrap: 'wrap',
+  },
+  filter: {
+    marginBottom: 10,
+    marginRight: 10,
+    height: 20,
+    borderRadius: 10,
+    backgroundColor: '#e5e5e5',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  filterTitle: {
+    fontSize: 9,
+    lineHeight: 10,
+    fontWeight: 'bold',
+    color: '#242323',
   },
 });
 
